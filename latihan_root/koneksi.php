cd9fb0efe5a7b0cb9fe85515c0412d25bddb022d
17 Gibran Raditya <gibranradityar@gmail.com>
2026-08-13 11:23:09 +0700

    Add files via upload

--- latihan_root/koneksi.php
+++ latihan_root/koneksi.php
@@ -2,7 +2,7 @@
 define('HOST_NAME', 'localhost');
 define('USER_NAME', 'root');
 define('PASSWORD', '');
-define('DB_NAME', 'latihan_root'); // Menyesuaikan nama database kamu
+define('DB_NAME', 'latihan_root');
 
 $koneksi = mysqli_connect(HOST_NAME, USER_NAME, PASSWORD, DB_NAME);
 
